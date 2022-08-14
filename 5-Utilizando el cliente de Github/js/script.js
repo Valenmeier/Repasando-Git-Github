b2574f2840391cf7de81430a0ce11c6bcbe9d318
Valentin Meier <valentinmeier123@gmail.com>
2022-08-14 14:41:55 -0300

    Guardar antes de hacer copia

--- 5-Utilizando el cliente de Github/js/script.js
+++ 5-Utilizando el cliente de Github/js/script.js
@@ -2,3 +2,14 @@
 //* Github es un servicio increiblemente flexible que se adapta a nuestras necesidades, podemos modificar los archivos de muchas formas de acuerdo a las necesidades que tenemos en el momento, ej: si necesitas modificar un contenido de manera rápida, puedes entrar a la página de Github y modificar los contenidos en cualquier parte del archivo. Puedes trabajar con la consola desde cualquier entorno, si quieres trabajar con un cliente grafico podemos usar el cliente de escritorio de github
 
 //? El cliente grafico tiene distintas opciones, la de clonar un repositorio, la de crear un repositorio dentro de mi equipo y la de subir un repositorio
+
+//* Debemos clonar un repositorio, abrirlo con el navegador y cambiar y guardar, luego de eso le damos arriba a push
+//* para sacarlo simplemente le damos click derecho y remove
+
+//Todo --> Crear repositorios locales en GIT
+//* File new repository
+//* Creando un nuevo repositorio va a ser como si estuviera utilizando Git, se va a guardar todo de manera local, para enviarlo a github tenemos que poner publish repository y se publica automaticamente
+
+
+//Todo --> sincronizando repositorios en Github
+//* Para sincronizar los cambios del repositorio que se encuentra online debemos hacer Fetch y pull origin
